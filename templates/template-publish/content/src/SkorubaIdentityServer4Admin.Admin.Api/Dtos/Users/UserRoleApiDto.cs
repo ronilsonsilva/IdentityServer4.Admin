e8211627269227b6d044cad895550b39217f40e2
Sousa Silva Engenharia de Sistemas <rs.ronilsonsilva@outlook.com.br>
2020-12-18 13:39:40 -0400

    a

---
+++ templates/template-publish/content/src/SkorubaIdentityServer4Admin.Admin.Api/Dtos/Users/UserRoleApiDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SkorubaIdentityServer4Admin.Admin.Api.Dtos.Users
+{
+    public class UserRoleApiDto<TKey>
+    {
+        public TKey UserId { get; set; }
+
+        public TKey RoleId { get; set; }
+    }
+}
+
+
+
+
+
---
+++ templates/template-publish/content/src/SkorubaIdentityServer4Admin.Admin/Configuration/Interfaces/IRootConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace SkorubaIdentityServer4Admin.Admin.Configuration.Interfaces
+{
+    public interface IRootConfiguration
+    {
+        AdminConfiguration AdminConfiguration { get; }
+        IdentityDataConfiguration IdentityDataConfiguration { get; }
+        IdentityServerDataConfiguration IdentityServerDataConfiguration { get; }
+    }
+}
+
+
+
+
+
